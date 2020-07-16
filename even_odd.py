2a88e1e72b8bc0017a0a566d2e511d4a438e8938
KanayBhandari <kanaybhandari2511@gmail.com>
2020-07-16 12:09:07 +0530

    modified even_odd which tell whether the number is single digit or multi-digit number

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,12 @@
-n = int(input("enter the no. you want to check whether it is even or odd : "))
+n = int(input("enter the positive no. you want to check whether it is even or odd : "))
 
 if n%2 == 0:
-    print("even")
+    if n <= 9:
+        print("single digit even number")
+    else:
+        print("not a single digit even number")
 else:
-    print("odd")
+    if n <= 9:
+        print("single digit odd number")
+    else:
+        print("not a single digit odd number")
